de53fd61a627e73b27ce070c46802094e7f56f44
YashRaj2017 <yashraj.vit01@gmail.com>
2021-05-22 20:04:42 +0530

    Latest

--- formApp.js
+++ formApp.js
@@ -1,39 +1,85 @@
+setupFavoriteList();
+
 const $form = $('form[name=search-songs]');
 
-$form.submit(function (e, theForm) {
-    e.preventDefault();
+$form.submit(function (evt, theForm) {
+    evt.preventDefault();
     const $input = $('input[type=text]', theForm);
     const searchTerm = encodeURIComponent($input.val());
-    // alert(searchTerm);
-    const url = ` https://itunes.apple.com/search?term=${searchTerm}&limit=10`;
+
+    const url = `https://itunes.apple.com/search?term=${searchTerm}&limit=10`;
 
     fetch(url)
         .then(function (resp) {
-            // console.log(resp);
-            return resp.json();
+            console.log(resp)
+            return resp.json()
         })
         .then(function (songData) {
-            console.log(songData)
-            // const html = buildList(songData.results);
-            $('#results ul').html(buildList(songData.results));
+            $('#results ul')
+                .html(buildList(songData.results));
+            $('#results ul')
+                .find('li.song').click(function (evt) {
+                    const favSongs = JSON.parse(localStorage.getItem('favSongs'));
+                    const favSong = $(evt.currentTarget).data();
 
+                    favSongs.push(favSong);
+                    localStorage.setItem('favSongs', JSON.stringify(favSongs));
+
+                    $('#favourites ul').html(buildList(favSongs));
+                })
         })
         .catch(function (err) {
-            alert("Error Occured!");
+            alert('ERROR!');
         })
 });
 
+
 function buildList(songs) {
     let rv = '';
+    if (!songs || !songs.length) {
+        return rv;
+    }
     for (let i = 0; i < songs.length; i++) {
         const item = songs[i];
         rv += `
-        <li class="song">
-            ${item.artistName} - ${item.collectionName}
-            <a href="${item.trackViewUrl}" target="_blank" ><img alt="Play" src="${item.artworkUrl30}"
-            width=30" height="30"></a>
-        </li>
-        `
+      <li class="song"
+        data-artist-name="${item.artistName}"
+        data-collection-name="${item.collectionName}"
+        data-artwork-url-100="${item.artworkUrl100 || item['artworkUrl-100']
+
+            }"
+:
+}"
+      >
+        ${item.artistName} - ${item.collectionName}
+
+        <a href="${item.trackViewUrl}" target="_blank" ><img src="${item.artworkUrl100 || item['artworkUrl-100']}" width=30" height="30" /></a>
+
+      </li>
+    `;
     }
+
     return rv;
 }
+
+
+
+
+
+
+function setupFavoriteList() {
+    const favSongs = localStorage.getItem('favSongs');
+    if (!favSongs) {
+        localStorage.setItem('favSongs', JSON.stringify(
+            []
+        ));
+    }
+    $('#favorites ul')
+        .html(buildList(JSON.parse(favSongs)))
+}
+
+
+
+
+
+
